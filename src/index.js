42283205f697ae8e88fe2f0513f78affcd44a669
IfzaRasool <ifzarasoolarain@gmail.com>
2022-04-06 00:39:51 +0300

    Add markup,style and js files - call data using Api in js - Add linter files - Add json package

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
 import './style.css';
+
+import { getScores, createScore } from './score.js';
+
+const refreshBtn = document.querySelector('.refresh-btn');
+const addBtn = document.querySelector('.add-btn');
+
+const GAME_ID = 'XcuVO085fpD8iKwa8mav';
+
+getScores(GAME_ID);
+
+window.addEventListener('load', () => {
+  refreshBtn.addEventListener('click', () => getScores(GAME_ID));
+  addBtn.addEventListener('click', () => createScore(GAME_ID));
+});
+
+// const createGame = async () => {
+//   await fetch(
+//     'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games',
+//     {
+//       method: 'POST',
+//       body: JSON.stringify({
+//         name: 'league_of_legends',
+//       }),
+//       headers: {
+//         'Content-type': 'application/json; charset=UTF-8',
+//       },
+//     }
+//   )
+//     .then((res) => res.json())
+//     .then((data) => console.log(data));
+// };
---
+++ src/score.js
@@ -0,0 +1,42 @@
+const ul = document.querySelector('.score-list');
+const userName = document.querySelector('.name');
+const gameScore = document.querySelector('.score');
+const BASE_URL =
+  'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games';
+
+// display score
+const displayScore = (data) => {
+  userName.value = '';
+  gameScore.value = '';
+  ul.innerHTML = '';
+  data.forEach((element) => {
+    const li = document.createElement('li');
+    li.innerHTML = `${element.user}: ${element.score}`;
+    ul.appendChild(li);
+  });
+};
+
+// fetch scores
+
+export const getScores = async (id) => {
+  await fetch(`${BASE_URL}/${id}/scores`)
+    .then((res) => res.json())
+    .then((data) => {
+      displayScore(data.result);
+    });
+};
+
+export const createScore = async (id) => {
+  await fetch(`${BASE_URL}/${id}/scores`, {
+    method: 'POST',
+    body: JSON.stringify({
+      user: userName.value,
+      score: gameScore.value,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  })
+    .then((res) => res.json())
+    .then((json) => json);
+};
